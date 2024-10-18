257fb20b0103964415f86c5bd405f9904a8760fc
Esther447 <e.mushimiyi@alustudent.com>
2024-10-18 08:37:12 -0700

    8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-str1 = "object"
-str2 = "-oriented"
-str3 = " programming"
-str4 = " with"
-str5 = " Python"
-
-print(str1 + str2 + str3 + str4 + str5)
+str1 = "object-oriented"
+str2 = " programming with"
+str3 = " Python"
+print(str1 + str2 + str3)
