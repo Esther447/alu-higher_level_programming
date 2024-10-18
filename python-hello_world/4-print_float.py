8d7ad1329465334bd6294c923b8e01ba55b735e0
Esther447 <e.mushimiyi@alustudent.com>
2024-10-18 05:24:23 -0700

    4-print_float.py

--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-number = 333.12  # Replace this with the number you want to print
-print(f"Float: {number:.2f}")  # Ensure two decimal places
+number = 3.14159  # You can change this to any float you want to test
+print(f"Float: {number:.2f}")
 
