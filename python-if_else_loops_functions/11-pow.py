9e7389887f13b75023c5b76c247895f62e824244
Esther447 <e.mushimiyi@alustudent.com>
2024-10-20 15:10:57 -0700

    11-pow.py

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 def pow(a, b):
-    return a ^ b
+    if b == 0:
+        return 1  # Any number to the power of 0 is 1
+    elif b < 0:
+        return 1 / pow(a, -b)  # Handle negative exponents
+    else:
+        result = 1
+        for _ in range(b):
+            result *= a
+        return result
