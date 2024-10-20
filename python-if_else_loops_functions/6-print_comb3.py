b9ae3491556728abee49867d24665b31982ce8d5
Esther447 <e.mushimiyi@alustudent.com>
2024-10-20 13:16:20 -0700

    6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
+        # Print the combination without a leading comma for the first combination
         if i == 0 and j == 1:
             print("{}{}".format(i, j), end="")
+        # Handle the last combination to avoid a trailing comma
+        elif i == 8 and j == 9:
+            print(", {}{}".format(i, j))
         else:
             print(", {}{}".format(i, j), end="")
 
