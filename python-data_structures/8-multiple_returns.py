20e4894ddf6c018d8c2f837542a5ebd9ba3ba9bc
Esther447 <e.mushimiyi@alustudent.com>
2024-10-24 14:16:39 -0700

    more returns

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-sentence = "At school, I learnt C!"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first)
+def multiple_returns(sentence):
+    # If the sentence is empty, return 0 and None as the first character
+    if sentence == "":
+        return (0, None)
+
+    # Otherwise, return the length of the sentence and the first character
+    return (len(sentence), sentence[0])
+
