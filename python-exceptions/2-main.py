ff9be3acc221d66aba151479530a2b2b6fdcc3dc
Esther447 <e.mushimiyi@alustudent.com>
2024-10-29 12:27:49 -0700

    Print and count integers

---
+++ python-exceptions/2-main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+safe_print_list_integers = \
+        __import__('2-safe_print_list_integers').safe_print_list_integers
+
+
+my_list = [1, 2, 3, 4]
+nb_print = safe_print_integers(my_list, len(my_list) + 4)
+print("nb_print: {:d}".format(nb_print))
+
+
+my_list = [1, 2, "H", 3, 4]
+nb_print = safe_print_list_integers(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+
+my_list = [1, 2, 3, "H", 4]
+nb_print = safe_print_list_integers(my_list, len(my_list) - 2)
+print("nb_print: {:d}".format(nb_print))
+
+my_list = ["Holberton", 1, 2, "H", 3, 4, [1, 2, 3, 4]
+nb_print = safe_print_list_integers(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
