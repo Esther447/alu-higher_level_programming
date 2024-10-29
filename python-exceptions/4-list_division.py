1abae696746094c3892532421d5184060e7b2412
Esther447 <e.mushimiyi@alustudent.com>
2024-10-29 12:47:35 -0700

    Divide a list

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     result = []
-    
+
     for i in range(list_length):
         try:
             num1 = my_list_1[i]
             num2 = my_list_2[i]
-
             result.append(num1 / num2)
 
         except IndexError:
