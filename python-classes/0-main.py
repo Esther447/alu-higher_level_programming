65f5abc001b96c015c50d4fc140adf43ba5f2c0a
Esther447 <e.mushimiyi@alustudent.com>
2024-10-30 16:18:26 -0700

    My first square

--- python-classes/0-main.py
+++ python-classes/0-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-Square = __impoort__('0-square').Square
+Square = __import__('0-square').Square
 
 my_square = Square()
 print(type(my_square))
--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
-    """
-    This module defines an empty Square class.
-    """
+"""Module that defines an empty Square class."""
 
-
-    class Square:
-        """
-
-        An empty class that represents a square.
-        """
-
-        pass
+class Square:
+    """An empty class that defines a square."""
+    pass
