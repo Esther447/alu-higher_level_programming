9367876ba25b04cbd7a68f1bb01dc977fbb05d23
Esther447 <e.mushimiyi@alustudent.com>
2024-10-30 10:26:21 -0700

    Area of a square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-This module defines a Square class with size validation and an area method."""
-
+This module defines a Square class with size validation and an area method.
+"""
 
 class Square:
     """
-    
+
     A class that represents a square.
     """
 
@@ -19,12 +19,12 @@ class Square:
              Size (int): The size is not an integer.
 
 
-        Raises:
+      Raises:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
         """
 
-        if not isinstance(size, int):
+       if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
@@ -34,7 +34,7 @@ class Square:
     def area(self):
         """
 
-        calculates the area of the square.
+       calculates the area of the square.
 
 
         Returns:
