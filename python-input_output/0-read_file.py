8f8121b26e76430c478e79bf9176e52bfa55e4dd
Esther447 <e.mushimiyi@alustudent.com>
2024-11-10 09:23:44 -0800

    Read file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,5 @@
+#!usr/bin/python3
+def read_file(filename=""):
+    """Reads a UTF-8 text and prints its content to stdout."""
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
