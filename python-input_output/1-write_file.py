de4f12e8296a453e4cf5ac784dce540339e350d5
Esther447 <e.mushimiyi@alustudent.com>
2024-11-10 10:06:00 -0800

    1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -4,6 +4,7 @@ This module contains a function that writes a string to a UTF-8 text file
 and returns the number of characters written.
 """
 
+
 def write_file(filename="", text=""):
     """Writes a string to a UTF-8 text file."""
     with open(filename, "w", encoding="utf-8") as f:
