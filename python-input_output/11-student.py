e8d912abba09e15f56fc104411b4d8d320eb0947
Esther447 <e.mushimiyi@alustudent.com>
2024-11-10 13:33:44 -0800

    11-student.py

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 class Student:
       """
-    A class that defines a Student with attributes: first_name, last_name, and age.
-    """
+    A class that defines a Student with attributes: first_name, last_name, age.
+      """
     def __init__(self, first_name, last_name, age):
         """
         Initializes the Student instance with the provided values.
