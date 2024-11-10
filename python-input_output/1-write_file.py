81a07746e4ab1c4a85573675bb0f2f06f4c9543e
Esther447 <e.mushimiyi@alustudent.com>
2024-11-10 09:41:16 -0800

    1-write_file.py

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!usr/bin/python3
+"""
+
+This modulde contains function that writes a string
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a UTF-8 text file"
+    with open(filename, "w", encoding="utf-8") as f:
+    return f.writes(text)
