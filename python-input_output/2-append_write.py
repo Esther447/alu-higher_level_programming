49e5f1d0805f657d6881e6ae6ee5cde6c78a9ac8
Esther447 <e.mushimiyi@alustudent.com>
2024-11-10 10:21:14 -0800

    to append a file

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -2,6 +2,7 @@
 """
 This module provides a function to append a string to a UTF-8 encoded text."""
 
+
 def append_write(filename="", text=""):
     """Appends a string to a UTF-8 text file."""
     with open(filename, "a", encoding="utf-8") as f:
