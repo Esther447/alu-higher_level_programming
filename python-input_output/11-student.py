0d2fd163c1d3a4329d613604b0480acfc9b197f3
Esther447 <e.mushimiyi@alustudent.com>
2024-11-11 03:53:32 -0800

    11-student.py

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,37 +1,23 @@
 #!/usr/bin/python3
-"""
-This module defines the Student class.
-"""
+"""This module defines a class Student"""
 
 
 class Student:
-    """
-    A class to represent a student
-    """
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """
-        Initializes the Student instance with first_name, last_name, age.
-        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """
-        Retrieves a dictionary representation of the Student instance.
-        """
-        if attrs is None:
-            return self.__dict__
-        if isinstance(attrs, list) 
-        and all(isinstance(attr, str) for attr in attrs):
-            return {attr: self.__dict__[attr] 
-                    for attr in attrs if attr in self.__dict__}
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
 
     def reload_from_json(self, json):
+        """Replaces all attributes of the Student
         """
-        Replaces all attributes of the Student instance based on a dictionary.
-        """
-        for key, value in json.items():
-            setattr(self, key, value)
+        for k, v in json.items():
+            setattr(self, k, v)
