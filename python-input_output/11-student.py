09b68dbaae608149f97707c0b7abcc8d87b85edc
Esther447 <e.mushimiyi@alustudent.com>
2024-11-11 03:41:18 -0800

    11-student.py

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,38 +1,17 @@
 #!/usr/bin/python3
 """
 This module defines the Student class.
-
-The Student class represents a student with attributes for first name, last name, and age.
-It provides methods for converting an instance to a dictionary representation (useful for JSON
-serialization) and for updating attributes from a dictionary (deserialization).
 """
+
 
 class Student:
     """
-    A class to represent a student.
-
-    Attributes:
-    -----------
-    first_name : str
-        The first name of the student.
-    last_name : str
-        The last name of the student.
-    age : int
-        The age of the student.
+    A class to represent a student
     """
 
     def __init__(self, first_name, last_name, age):
         """
-        Initializes the Student instance with first_name, last_name, and age.
-
-        Parameters:
-        -----------
-        first_name : str
-            The first name of the student.
-        last_name : str
-            The last name of the student.
-        age : int
-            The age of the student.
+        Initializes the Student instance with first_name, last_name, age.
         """
         self.first_name = first_name
         self.last_name = last_name
@@ -41,18 +20,6 @@ class Student:
     def to_json(self, attrs=None):
         """
         Retrieves a dictionary representation of the Student instance.
-
-        Parameters:
-        -----------
-        attrs : list, optional
-            A list of attribute names to include in the dictionary (default is None).
-
-        Returns:
-        --------
-        dict
-            A dictionary representation of the Student instance.
-            If attrs is a list of strings, only attributes listed in attrs are included.
-            Otherwise, all attributes are included.
         """
         if attrs is None:
             return self.__dict__
@@ -63,12 +30,6 @@ class Student:
     def reload_from_json(self, json):
         """
         Replaces all attributes of the Student instance based on a dictionary.
-
-        Parameters:
-        -----------
-        json : dict
-            A dictionary where each key is an attribute name, and each value is
-            the new value for the attribute.
         """
         for key, value in json.items():
             setattr(self, key, value)
