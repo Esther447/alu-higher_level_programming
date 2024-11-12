c0bfbb93d17988de80e3e4d2c7ebcb45187ee589
Esther447 <e.mushimiyi@alustudent.com>
2024-11-12 05:46:03 -0800

    1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,10 @@
---1-create_user.sql
+-- 1-create_user.sql
+
+-- Create the user 'user_0d_1' if it does not already exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all global privileges to 'user_0d_1' on the MySQL server
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Apply changes immediately
 FLUSH PRIVILEGES;
