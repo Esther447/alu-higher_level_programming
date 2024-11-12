47a7da4288654097ee4a6160ca1adeefca63ac09
Esther447 <e.mushimiyi@alustudent.com>
2024-11-12 06:00:17 -0800

    0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,15 +1,13 @@
--- Check if user_0d_1 exists and display their grants if they do
-SELECT 'Checking privileges for user_0d_1@localhost' AS Info;
-IF EXISTS (SELECT 1 FROM mysql.user WHERE user = 'user_0d_1' AND host = 'localhost') THEN
-    SHOW GRANTS FOR 'user_0d_1'@'localhost';
-ELSE
-    SELECT 'There is no such grant defined for user user_0d_1 on host localhost' AS Result;
-END IF;
+-- Create user_0d_1 if it does not exist and grant privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 
--- Check if user_0d_2 exists and display their grants if they do
-SELECT 'Checking privileges for user_0d_2@localhost' AS Info;
-IF EXISTS (SELECT 1 FROM mysql.user WHERE user = 'user_0d_2' AND host = 'localhost') THEN
-    SHOW GRANTS FOR 'user_0d_2'@'localhost';
-ELSE
-    SELECT 'There is no such grant defined for user user_0d_2 on host localhost' AS Result;
-END IF;
+-- Create user_0d_2 if it does not exist and grant privileges
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+
+-- List privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- List privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
