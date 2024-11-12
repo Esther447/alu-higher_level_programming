1860ee9e024263690064ecc9cd0e8607263d5881
Esther447 <e.mushimiyi@alustudent.com>
2024-11-12 15:10:42 -0800

    2-create_read_user.sql

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,10 @@
--- create the database if it does not already exit
-CREATE DATABASE IF NOT EXITS hbtn_0d_2;
-CREATE USER IF NOT EXITS 'user_0d_2'@'localhost' IDENTIFIED BY ''user_od_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
-FLUSH PRIVILEGES;
+-- creates the database hbtn_0d_2 and the user user_0d_2
+-- The usr_0d_2 has SELECT privileges on hbtn_0d_2 with password user_0d_2pwd
+CREATE DATABASE
+    IF NOT EXISTS 'hbtn_0d_2';
+CREATE USER
+    IF NOT EXISTS 'user_0d_2@'localhost'
+    IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT
+   ON 'hbtn_0d_2'.*
+   TO 'user_0d_2'@'localhost';
