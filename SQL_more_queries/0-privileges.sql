bf143965622b696339229d3f80b56615d533cf65
Esther447 <e.mushimiyi@alustudent.com>
2024-11-12 14:28:50 -0800

    0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,16 +1,3 @@
--- Create users if they do not exist
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'password';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'password';
-
--- Grant privileges to each user
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';
-
--- Ensure privileges are updated
-FLUSH PRIVILEGES;
-
--- List privileges for user_0d_1
+-- Lists all privileges of the users user_0d_1 and user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- List privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
