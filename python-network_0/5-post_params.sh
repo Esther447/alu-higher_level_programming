a9b4ece6b4e01ff32d26fd9750601102d903bd11
Esther447 <e.mushimiyi@alustudent.com>
2024-11-21 09:00:27 +0000

    5-post_params.sh

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #This script sends a POST request to the passed URL, and displays the body of the response
-curl -s -X POST -d "email=e.mushimiyi@gmail.com&subject=I will always be here for PLD" "$1"
+curl -s -X POST -d "email=e.mushimiyi" -d "gmail.com&subject=I will always be here for PLD" "$1"
