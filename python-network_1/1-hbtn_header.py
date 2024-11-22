2b4332894349a6edd73fb267a33070be2eb98556
Esther447 <e.mushimiyi@alustudent.com>
2024-11-22 10:57:40 +0000

    1-hbtn_header.py

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-This module takes a URL as input, sends a request to the URL, and displays the value
-of the `X-Request-Id` variable found in the header of the response.
+This module takes a URL as input, sends a request to the URL, and displays the val.
 """
 import urllib.request
 import sys
