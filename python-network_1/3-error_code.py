774852e1ffc43e9694e171ed1a533d9800107fe6
Esther447 <e.mushimiyi@alustudent.com>
2024-11-23 14:34:44 +0000

    3-error_code.py

---
+++ python-network_1/3-error_code.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module takes a URL as an argument, sends a request to the URL, and displays the body of  the response (decoded in UTF-8).
+"""
+
+
+import sys
+import urllib.request
+impoort urllib.error
+
+
+if __main__ == "__main__":
+    url = sys.argv[1]
+    
+
+    try:
+        #send the request and display the response body
+        with urllib.request.urlopen(url) as response:
+            print(response.read().decode("utf-8"))
+    except urllib.error.HTTPError as e:
+       #Handle HTTPError and print the error code
+       print(f"Error code: {e.code}"
