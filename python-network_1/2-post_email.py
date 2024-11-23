24b2d47d58f9d3a1430d64490b3cd9cd80433c99
Esther447 <e.mushimiyi@alustudent.com>
2024-11-23 13:07:16 +0000

    2-post_email.py

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import sys
+import urllib.request
+import urllib.parse
+
+
+def post_email():
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+
+    data = {'email': email}
+
+
+    encoded_data = urllib.parse.urlencode(data).encode('utf-8')
+
+
+    with urllib.request.urlopen(url, encoded_data) as response:
+        body = response.read().decode('utf-8)
+
+
+    print("Your email is: {}".format(email))
+    print(body)
+
+
+if __name__ == "__main__":
+    post_email(
