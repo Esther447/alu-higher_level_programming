feed325f713c2bfc123a8703da95014308118816
Esther447 <e.mushimiyi@alustudent.com>
2024-11-24 14:30:22 +0000

    6-post_email.py

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -2,12 +2,13 @@
 """A script that:
 - takes in a URL,
 - sends a request to the URL and displays the value
-- of the X-Request-Id variable found in the header of the response"""
+- of the X-Request-Id variable found in the header ofthe response.
+"""
 import sys
 import requests
 
-if __name__ == " __main__":
+if __name__ == "__main__":
 
     new_value = {'email': sys.argv[2]}
-    req = requests.post9sys.argv[1], new_value)
+    req = requests.post(sys.argv[1], new_value)
     print(req.text)
