b61078ffdac555cb13ec5393ae9dae9cc90077dd
Esther447 <e.mushimiyi@alustudent.com>
2024-11-24 13:43:17 +0000

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""I did it very well"""
-mport requests
+"""
+Python script that fetches https://alu-intranet.hbtn.io/status
+"""
+import requests
 
+if __name__ == '__main__':
+    url = 'https://intranet.hbtn.io/status'
+    if url.startswith('https://'):
+        url = "https://alu-intranet.hbtn.io/status"
 
-if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
-    if url.startswitch('https://'):
-        url = "https://alu-intranet.hbtn.io/statu"
-
-
-    response = requests.get(url
+    res = requests.get(url)
     print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
