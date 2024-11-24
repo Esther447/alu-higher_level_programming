4262c64bbc299ebbfa8ca381a02ef57040599678
Esther447 <e.mushimiyi@alustudent.com>
2024-11-24 14:23:15 +0000

    6-post_email.py

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -4,6 +4,8 @@
 - sends a request to the URL and displays the value
 - of the X-Request-Id variable found in the header of the response
 """
+import sys
+import requests
 
 if __name__ == " __main__":
 
