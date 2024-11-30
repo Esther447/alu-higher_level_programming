49afdfd9c8990c95b967bb101552abf66cdd50c0
Esther447 <e.mushimiyi@alustudent.com>
2024-11-30 23:12:20 +0000

    2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,47 +1,27 @@
 #!/usr/bin/python3
 """
-Script that takes in arguments and displays all values in the
-states table of a database where the name matches the given argument.
-It uses the MySQLdb module and constructs the query using format.
+Script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument
 """
-
-import sys
 import MySQLdb
+from sys import argv
 
-if __name__ == "__main__":
-    # Check if sufficient arguments are provided
-    if len(sys.argv) != 5:
-        print("Usage: ./script.py <mysql_user> <mysql_password> <database_name> <state_name>")
-        sys.exit(1)
+# The code should not be executed when imported
+if __name__ == '__main__':
 
-    try:
-        # Connect to the MySQL database
-        conn = MySQLdb.connect(
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3],
-            host="localhost",
-            port=3306
-        )
-        cursor = conn.cursor()
+    # make a connection to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
 
-        # Construct the query using format
-        query = """
-        SELECT * FROM states
-        WHERE name LIKE BINARY '{}'
-        ORDER BY id ASC
-        """.format(sys.argv[4])
-        cursor.execute(query)
+    # It gives us the ability to have multiple seperate working environments
+    # through the same connection to the database.
+    cur = db.cursor()
+    nmeSr = "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(argv[4])
+    cur.execute(nmeSr)
 
-        # Fetch and display results
-        results = cursor.fetchall()
-        for row in results:
-            print(row)
-
-    except MySQLdb.Error as err:
-        print(f"Error: {err}")
-
-    finally:
-        # Ensure resources are closed
-        cursor.close()
-        conn.close()
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+    # Clean up process
+    cur.close()
+    db.close()
