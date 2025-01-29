05df6fc2b60b89349e42de557e40315acc7fedce
esther447 <e.mushimiyi@alustudent.com>
2025-01-29 10:34:33 +0200

    3-rectangle.js

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -8,14 +8,6 @@ class Rectangle {
     }
   }
 
-print() {
-  constructor(w, h) {
-    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
   print() {
     if (this.width && this.height) {
       for (let i = 0; i < this.height; i++) {
