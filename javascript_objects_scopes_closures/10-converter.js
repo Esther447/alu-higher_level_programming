2274db4308a83bbd8444a40632ef37648674bf41
esther447 <e.mushimiyi@alustudent.com>
2025-01-29 23:26:38 +0200

    10-converter.js

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -1,15 +1,8 @@
-#!/usr/bin/node
-
-exports.converter = function (base) {
-  return function (number) {
-    // Convert the number to the desired base
-    let result = number.toString(base);
-
-    // Ensure that there's no leading zero when the base is 10
-    if (base === 10 && result[0] === '0' && result.length > 1) {
-      result = result.slice(1);
-    }
-
+// Using let correctly in a function
+let number = 10;
+function converter(base) {
+  return function (num) {
+    let result = num.toString(base); // Declare result variable inside the function scope
     return result;
   };
-};
+}
