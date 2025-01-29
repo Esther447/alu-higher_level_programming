fc5c547519da69433934fde59f38c0f966842724
esther447 <e.mushimiyi@alustudent.com>
2025-01-29 23:21:17 +0200

    10-converter.js

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -3,6 +3,6 @@
 exports.converter = function (base) {
   return function (number) {
     if (number === 0) return '0';
-    return exports.converter(base)(Math.floor(number / base)) + (number % base).toString(base);
+    return exports.converter(base)(Math.floor(number / base)) + (number % base).toString(base).replace(/^0+/, '');  // Remove leading zeros
   };
 };
