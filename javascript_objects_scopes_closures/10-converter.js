90859e814843c4f984daec12e65174686ebe1e2a
esther447 <e.mushimiyi@alustudent.com>
2025-01-29 23:13:01 +0200

    10-converter.js

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -2,12 +2,6 @@
 
 exports.converter = function (base) {
   return function (number) {
-    if (number === 0) return '0';
-    let result = '';
-    while (number > 0) {
-      result = (number % base).toString(base) + result;
-      number = Math.floor(number / base);
-    }
-    return result;
+    return (number === 0) ? '0' : (exports.converter(base)(Math.floor(number / base))
   };
 };
